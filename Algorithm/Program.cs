8b1842ab8671a5dfb556c502516247ac1218f9e8
Vilia Li <vilia@Vilias-MacBook-Air.local>
2019-01-06 13:01:31 +1300

    initial commit

---
+++ Algorithm/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Algorithm
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
